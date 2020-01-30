0114d883d5dc7a91cde6cdd630a1d9a7434f737f
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-01-31 03:30:08 +0530

    Arduino Code

---
+++ SONAR_Arduino_code.ino
@@ -0,0 +1,58 @@
+#include <Servo.h>
+
+float d1,d2,d,angle;
+int trigpin=5,echopin=6;
+Servo myservo;  
+
+int pos = 0;    
+
+void setup() {
+  myservo.attach(9);  
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 10) 
+  {
+    d1=calcdistance();
+    myservo.write(pos);
+    angle=pos;   
+    d2=calcdistance();
+
+    d=(d1+d2)/2;
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.print(d);
+    Serial.println(",");
+    delayMicroseconds(10);                       
+  }
+  for (pos = 180; pos >= 0; pos -= 10) 
+  { 
+    d1=calcdistance();
+    myservo.write(pos); 
+    angle=pos;  
+    d2=calcdistance();
+
+    d=(d1+d2)/2;
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.print(d);
+    Serial.println(",");
+    delayMicroseconds(10);                  
+  }
+}
+
+float calcdistance()
+{ 
+  float duration,distance;
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,LOW);
+
+  duration=pulseIn(echopin,HIGH);
+
+  distance=(duration/2)*0.0343;
+  return distance;
+}
